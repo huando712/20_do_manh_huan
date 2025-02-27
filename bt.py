192e2e9282da9e56f01f2601c5214f69271e7dfc
huando712 <dmhuan24174600112@sv.uneti.edu.vn>
2025-02-27 15:06:53 +0700

    mbt.mbtpy

--- bt.py
+++ bt.py
@@ -98,62 +98,62 @@ else:
     thue=0.1
 thue_thu_nhap=n*thue
 luong_rong=n-thue_thu_nhap
-print(f"thuế thu nhập là:{thue_thu_nhap}")
-print(f"lương ròng là:{luong_rong}")
+# print(f"thuế thu nhập là:{thue_thu_nhap}")
+# print(f"lương ròng là:{luong_rong}")
 
 
-n=input("nhập vào tháng")
-if n==1:
-    print("tháng 1 có 31 ngày")
-elif n==2:
-    print("tháng 2 có 28 ngày")
-elif n==3:
-    print("tháng 3 có 31 ngày")
-elif n==4:
-    print("tháng 4 có 30 ngày")
-elif n==5:
-    print("tháng 5 có 31 ngày")
-elif n==6:
-    print("tháng 6 có 30 ngày")
-elif n==7:
-    print("tháng 7 có 31 ngày")
-elif n==8:
-    print("tháng 8 có 31 ngày")
-elif n==9:
-    print("tháng 9 có 30 ngày")
-elif n==10:
-    print("tháng 10 có 31 ngày")
-elif n==11:
-    print("tháng 11 có 30 ngày")
-elif n==12:
-    print("tháng 12 có 31 ngày")
-else:
-    print("nhập sai")
+# n=input("nhập vào tháng")
+# if n==1:
+#     print("tháng 1 có 31 ngày")
+# elif n==2:
+#     print("tháng 2 có 28 ngày")
+# elif n==3:
+#     print("tháng 3 có 31 ngày")
+# elif n==4:
+#     print("tháng 4 có 30 ngày")
+# elif n==5:
+#     print("tháng 5 có 31 ngày")
+# elif n==6:
+#     print("tháng 6 có 30 ngày")
+# elif n==7:
+#     print("tháng 7 có 31 ngày")
+# elif n==8:
+#     print("tháng 8 có 31 ngày")
+# elif n==9:
+#     print("tháng 9 có 30 ngày")
+# elif n==10:
+#     print("tháng 10 có 31 ngày")
+# elif n==11:
+#     print("tháng 11 có 30 ngày")
+# elif n==12:
+#     print("tháng 12 có 31 ngày")
+# else:
+#     print("nhập sai")
 
 
-def doc_so_3_chu_so(n):
-    hang_tram = ["", "Một trăm", "Hai trăm", "Ba trăm", "Bốn trăm", "Năm trăm",
-                 "Sáu trăm", "Bảy trăm", "Tám trăm", "Chín trăm"]
-    hang_chuc = ["", "mười", "hai mươi", "ba mươi", "bốn mươi", "năm mươi",
-                 "sáu mươi", "bảy mươi", "tám mươi", "chín mươi"]
-    hang_dv = ["", "một", "hai", "ba", "bốn", "năm", "sáu", "bảy", "tám", "chín"]
+# def doc_so_3_chu_so(n):
+#     hang_tram = ["", "Một trăm", "Hai trăm", "Ba trăm", "Bốn trăm", "Năm trăm",
+#                  "Sáu trăm", "Bảy trăm", "Tám trăm", "Chín trăm"]
+#     hang_chuc = ["", "mười", "hai mươi", "ba mươi", "bốn mươi", "năm mươi",
+#                  "sáu mươi", "bảy mươi", "tám mươi", "chín mươi"]
+#     hang_dv = ["", "một", "hai", "ba", "bốn", "năm", "sáu", "bảy", "tám", "chín"]
 
-    t, c, d = n // 100, (n // 10) % 10, n % 10
-    print(f"{hang_tram[t]} {'lẻ' if c == 0 and d else hang_chuc[c]} {'lăm' if d == 5 and c else hang_dv[d]}".strip())
+#     t, c, d = n // 100, (n // 10) % 10, n % 10
+#     print(f"{hang_tram[t]} {'lẻ' if c == 0 and d else hang_chuc[c]} {'lăm' if d == 5 and c else hang_dv[d]}".strip())
 
-n = int(input("Nhập số nguyên có 3 chữ số: "))
-doc_so_3_chu_so(n)
+# n = int(input("Nhập số nguyên có 3 chữ số: "))
+# doc_so_3_chu_so(n)
 
 
-tnct=int(input("nhập thâm niên công tác"))
-luong_co_ban=1350000
-if tnct<12:
-    he_so=2.34
-elif 12<= tnct<36:
-    he_so=3.33
-elif 36<= tnct <60:
-    he_so=3.36
-else:
-    he_so=3.99
-luong=he_so*luong_co_ban
-print(f"lương của nhân viên là : {luong}")
+# tnct=int(input("nhập thâm niên công tác"))
+# luong_co_ban=1350000
+# if tnct<12:
+#     he_so=2.34
+# elif 12<= tnct<36:
+#     he_so=3.33
+# elif 36<= tnct <60:
+#     he_so=3.36
+# else:
+#     he_so=3.99
+# luong=he_so*luong_co_ban
+# print(f"lương của nhân viên là : {luong}")
